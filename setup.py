4eb72e58011a7b3fce5f861c59ebc84901966fff
Bradford <1217269+elBradford@users.noreply.github.com>
2017-02-13 17:02:31 -0500

    * Python Packaging Structure (https://python-packaging.readthedocs.io/en/latest/minimal.html#creating-the-scaffolding) * Added test RSA keys

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='privledge',
+      version='0.1',
+      description='Private, Privleged Ledger',
+      long_description=readme(),
+      url='https://github.com/elBradford/privledge',
+      author='Bradford',
+      packages=['privledge'],
+      install_requires=[
+          'sshpubkeys',
+          'python-daemon',
+          'termcolor',
+      ],
+      entry_points = {
+          'console_scripts': ['pls=privledge.cli:shell'],
+      },
+      zip_safe=False)
